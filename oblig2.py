56abe82cda5e5c0498e756362d7e0b426f247821
Joshyking06 <joshuai@afk.no>
2026-03-17 19:03:46 +0100

    all done with code

--- oblig2.py
+++ oblig2.py
@@ -1,8 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import math as m
 
 
 def f(x):
-    return (m.e(-x/4))*np.arctan(x)
+    return (np.exp(-x/4))*np.arctan(x)
+
+def g(x):
+    return np.arctan(x) - 4/(x**2+1)
+
+x = np.linspace(0, 5, 500)
+
+x_topp = 1.6907
+y_topp = f(x_topp)
+
+#plotter f(x)
+plt.plot(x, f(x))
+plt.title("f(x)")
+
+#plotter punkt toppunkt
+plt.scatter(x_topp, y_topp)
+
+#x og y streker 
+plt.axhline(0, color="black")
+plt.axvline(0, color="black")
+
+plt.show()
+
+# plotte den deriverte
+plt.plot(x, g(x))
+
+plt.scatter(x_topp, 0)
+
+plt.axhline(0, color="black")
+plt.axvline(0, color="black")
+
+plt.show()
+
+
 
